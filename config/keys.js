24f8d202e97634bdaf36e82696c8a83d3958a9e5
Martin <luther.perdana@gmail.com>
2019-01-15 14:35:37 +0700

    Finished user api route

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://xiu:xiurong25@ds151124.mlab.com:51124/merndb'
+    mongoURI: 'mongodb://xiu:xiurong25@ds151124.mlab.com:51124/merndb',
+    secretOrKey: 'secret'
 }
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
+
+//Body Parser middleware
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
@@ -17,7 +25,12 @@ mongoose.connect(db, {
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello!!'));
+// app.get('/', (req, res) => res.send('Hello!!'));
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport Config
+require('./config/passport')(passport);
 
 // Use Routes
 app.use('/api/users', users);
