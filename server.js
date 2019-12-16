bf66f5eba5d8c229ca0937626430fe984581cb6c
Martin <luther.perdana@gmail.com>
2019-12-16 16:10:16 +0700

    TESTING PUSH 2

--- server.js
+++ server.js
@@ -10,20 +10,23 @@ const posts = require('./routes/api/posts');
 const app = express();
 
 //Body Parser middleware
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+);
 app.use(bodyParser.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
-mongoose.connect(db, {
-        useNewUrlParser: true
-    })
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
+mongoose
+	.connect(db, {
+		useNewUrlParser: true
+	})
+	.then(() => console.log('MongoDB Connected'))
+	.catch(err => console.log(err));
 
 // app.get('/', (req, res) => res.send('Hello!!'));
 
@@ -37,7 +40,7 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-console.log('TESTING PUSH');
+console.log('TESTING PUSH 2');
 
 const port = process.env.PORT || 5000;
 
